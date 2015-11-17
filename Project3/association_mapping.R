79ecde99bd2987514f589c15022452d3df673235
Will Spurgin <will.spurgin@gmail.com>
2015-11-16 23:49:10 -0600

    Finish Project 3
    Made association mapping code more generic (for deployment).

--- Project3/association_mapping.R
+++ Project3/association_mapping.R
@@ -1,4 +1,4 @@
-# Usage Rscript path/to/cleaning.R Census_Data_2010.csv Graduation_Rates.csv Max_Tract_Overlap.csv F33-finance.csv
+# Usage Rscript path/to/association_mapping.R Census_Data_2010.csv Graduation_Rates.csv Max_Tract_Overlap.csv F33-finance.csv
 
 # Will Spurgin
 # Copyright November 7th, 2015
@@ -46,30 +46,52 @@ barplot(table(discretized.districts$all.rate) / nrow(discretized.districts), mai
 
 # Association Rule Mapping ------------------------------------------------
 
-trans <- as(discretized.districts, "transactions")
-trans
+national.trans <- as(discretized.districts, "transactions")
+national.trans
 
 # Only contains with all.rate in the right hand side (should be around 473K)
-r <- apriori(trans, parameter = list(support=0.02),
+r.national <- apriori(trans, parameter = list(support=0.02),
              appearance = list(rhs = c("all.rate=low",
                                        "all.rate=medium",
                                        "all.rate=high"),
                                default = "lhs"))
 
-
-inspect(head(sort(r, by = "lift")))
-
-plot(r)
-
 m <- interestMeasure(r, measure = c("chiSquared", "fishersExactTest"),
                      transactions = trans)
 quality(r) <- cbind(quality(r), m)
-inspect(head(r))
 
-inspect(head(sort(r, by = "fisher", decreasing = FALSE)))
-inspect(head(sort(r, by = "fisher", decreasing = FALSE)))
-plot(head(sort(r, by = "fisher", decreasing = FALSE), n = 50), method = "graph", interactive = TRUE)
+texas.discretized.districts <- districts[districts$state.name == "Texas", ] %>%
+  select(-one_of("gidtr", "district.id", "percentage", "state.name"))
+for ( i in 1:ncol(discretized.districts))
+  texas.discretized.districts[, i] <- discretize(texas.discretized.districts[, i], method = "frequency", categories = 3,labels = c("low", "medium", "high"))
 
-r2 <- subset(r, rhs %pin% "all.rate=high")
-inspect(head(sort(r2, by = "fisher", decreasing = FALSE)))
-plot(head(sort(r2, by = "fisher", decreasing = FALSE), n = 50), method = "graph", interactive = TRUE)
+texas.trans <- discretized.districts %>% as("transactions")
+tecas.trans
+
+r.low <- apriori(trans, parameter = list(support=0.05),
+                  appearance = list(rhs = c("all.rate=medium"),
+                                    default = "lhs"))
+
+m <- interestMeasure(r.low, measure = c("chiSquared", "fishersExactTest"),
+                     transactions = texas.trans)
+quality(r.low) <- cbind(quality(r.low), m)
+
+r.medium <- apriori(trans, parameter = list(support=0.05),
+             appearance = list(rhs = c("all.rate=medium"),
+                               default = "lhs"))
+
+m <- interestMeasure(r.medium, measure = c("chiSquared", "fishersExactTest"),
+                     transactions = texas.trans)
+quality(r.medium) <- cbind(quality(r.medium), m)
+
+r.high <- apriori(trans, parameter = list(support=0.05),
+                    appearance = list(rhs = c("all.rate=medium"),
+                                      default = "lhs"))
+
+m <- interestMeasure(r.high, measure = c("chiSquared", "fishersExactTest"),
+                     transactions = texas.trans)
+quality(r.high) <- cbind(quality(r.high), m)
+
+rm(m, i)
+# Plot your associations using the following command. Substitute "r" with your Formal class rules' object.
+# plot(head(sort(r, by = "fisher", decreasing = FALSE), n = 50), method = "graph", interactive = TRUE)
